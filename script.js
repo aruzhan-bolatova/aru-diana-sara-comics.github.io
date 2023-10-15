b2287851b30b1523a19895ea06986d4d9f52dd3e
sarabindalmook <145615356+sarabindalmook@users.noreply.github.com>
2023-10-15 20:27:23 +0400

    Update Audio script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
- AOS.init();
+AOS.init();
+
+const playButton = document.getElementById("playButton");
+const bgMusic = document.getElementById("bgMusic");
+
+ playButton.addEventListener("click", () => {
+     if (bgMusic.paused) {
+         bgMusic.play();
+         playButton.textContent = "Pause Music";
+     } else {
+         bgMusic.pause();
+         playButton.textContent = "Play Music";
+     }
+ });
